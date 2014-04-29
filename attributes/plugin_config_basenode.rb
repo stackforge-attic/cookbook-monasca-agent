1e1ae69df51549902613c7e55ddc4638ae8c20ec
Tim Kuhlman <tim.kuhlman@hp.com>
2014-04-29 15:58:40 -0600

    removed mon_ prefix from plugins

--- attributes/plugin_config_basenode.rb
+++ attributes/plugin_config_basenode.rb
@@ -2,13 +2,13 @@
 
 # Host alive check for api server
 
-node.default[:mon_agent][:plugin][:mon_host_alive][:init_config] = {
+node.default[:mon_agent][:plugin][:host_alive][:init_config] = {
   :ssh_port => 22,
   :ssh_timeout => 0.5,
   :ping_timeout => 1
 }
 
-node.default[:mon_agent][:plugin][:mon_host_alive][:instances][:api_with_ssh] = {
+node.default[:mon_agent][:plugin][:host_alive][:instances][:api_with_ssh] = {
   :host_name => "192.168.10.4",
   :alive_test => "ssh"
 }
@@ -30,17 +30,17 @@ node.default[:mon_agent][:plugin][:process][:instances][:ntpd] = {
 
 # Service checks: For demo, everyone can check mon api
 
-node.default[:mon_agent][:plugin][:mon_http_check][:init_config] = {
+node.default[:mon_agent][:plugin][:http_check][:init_config] = {
 }
 
-node.default[:mon_agent][:plugin][:mon_http_check][:instances][:monapi] = {
+node.default[:mon_agent][:plugin][:http_check][:instances][:monapi] = {
   :name => "mon_api",
   :url => "http://192.168.10.4:8080",
   :timeout => 10,
   :collect_response_time => "true",
   :match_pattern => '.*status.*CURRENT.*',
 }
-node.default[:mon_agent][:plugin][:mon_http_check][:instances][:monapi_health] = {
+node.default[:mon_agent][:plugin][:http_check][:instances][:monapi_health] = {
   :name => "mon_api_health",
   :url => "http://192.168.10.4:8081/healthcheck",
   :timeout => 10,
@@ -51,13 +51,12 @@ node.default[:mon_agent][:plugin][:mon_http_check][:instances][:monapi_health] =
 
 # mon_nagios_wrapper checks: For demo, everyone can check disk
 #
-#node.default[:mon_agent][:plugin][:mon_nagios_wrapper][:init_config] = {
+#node.default[:mon_agent][:plugin][:nagios_wrapper][:init_config] = {
 #  :check_path => "/usr/lib/nagios/plugins:/usr/local/bin/nagios"
 #}
 #
-#node.default[:mon_agent][:plugin][:mon_nagios_wrapper][:instances][:check_disk] = {
+#node.default[:mon_agent][:plugin][:nagios_wrapper][:instances][:check_disk] = {
 #  :service_name => "disk",
 #  :check_command => "check_disk -w 15\\% -c 5\\% -A -i /srv/node",
 #  :check_interval => 300
 #}
-
