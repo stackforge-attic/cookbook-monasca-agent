e025e32676745aa945e496c34e108206e0445ae0
Tim Kuhlman <tim.kuhlman@hp.com>
2014-04-30 11:32:58 -0600

    removed custom emitters

--- metadata.rb
+++ metadata.rb
@@ -4,9 +4,4 @@ maintainer_email "hpcs-mon@hp.com"
 description      "Installs/Configures mon-agent components"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "1.0.1"
-
-supports         "ubuntu"
-
-suggests         "python"
-
 recipe "mon_api::default", "Default"
