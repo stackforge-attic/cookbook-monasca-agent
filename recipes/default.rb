f412aafaf370dab3569ec1837c79b78aa2395cb7
Tim Kuhlman <tim.kuhlman@hp.com>
2014-06-11 10:08:34 -0600

    Fixed typo in data bag name

--- recipes/default.rb
+++ recipes/default.rb
@@ -12,8 +12,8 @@ python_pip 'mon-agent' do
 end
 
 # todo what is with the multiple levels and the repetition in the names in the data bag, fix
-# todo service is a new setting in the databag
-setting = data_bag_item(node[:mon_agent][:data_bag], 'mon_agent')
+# todo service is a new settings in the databag
+settings = data_bag_item(node[:mon_agent][:data_bag], 'mon_agent')
 execute 'mon-setup' do
   action :run
   cmd "/usr/local/bin/mon-setup -u #{settings['username']} -p #{settings['password']} -s #{settings['service']} --keystone_url #{settings['keystone_url']} --project_name #{settings['project_name']} --mon_url #{settings['mon_api_url']}"
