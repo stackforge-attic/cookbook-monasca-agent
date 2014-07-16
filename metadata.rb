528d8732a0193bd31af22973a242458d39bf01f4
Craig Bryant <craig.bryant@hp.com>
2014-07-16 17:50:16 -0600

    Get initial build to work.
    Add .gitreview, Gemfile, .rubocop.yml, Berksfile, Strainerfile
    
    Fixed style and lint errors.
    
    Added empty unit test so chef-unit passes until we get real unit tests
    
    Change-Id: I8f2e48782a5289c82b0d4fc147d95f637be046c6

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,10 @@
-name             "mon_agent"
-maintainer       "HP_Cloud_Monitoring"
-maintainer_email "hpcs-mon@hp.com"
-description      "Installs/Configures mon-agent components"
+# encoding: UTF-8#
+#
+name             'mon_agent'
+maintainer       'HP_Cloud_Monitoring'
+maintainer_email 'hpcs-mon@hp.com'
+description      'Installs/Configures mon-agent components'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.1.1"
+version          '1.1.1'
 depends          'python'
-recipe "mon_api::default", "Default"
+recipe 'mon_api::default', 'Default'
--- recipes/plugin_cfg.rb
+++ recipes/plugin_cfg.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8#
 #
 # Cookbook Name:: mon_agent
 # Recipe:: plugin_cfg
@@ -6,25 +7,25 @@
 # Load nagios-plugins package if it's needed
 package 'nagios-plugins-basic' do
   action :install
-  only_if { node[:mon_agent][:plugin].has_key?(:nagios_wrapper) }
+  only_if { node[:mon_agent][:plugin].key?(:nagios_wrapper) }
 end
 
 # Configures the plugin yaml files based on node[:mon_agent][:plugin]
 # attributes
 node[:mon_agent][:plugin].each_key do |plugin|
-  if not node[:mon_agent][:plugin][plugin].has_key?(:init_config)
-    node[:mon_agent][:plugin][plugin][:init_config] = {}
+  unless node[:mon_agent][:plugin][plugin].key?(:init_config)
+    node.normal[:mon_agent][:plugin][plugin][:init_config] = {}
   end
   template "/etc/mon-agent/conf.d/#{plugin}.yaml" do
-    source "plugin_yaml.erb"
+    source 'plugin_yaml.erb'
     action :create
-    owner node['mon_agent']['owner']
-    group node['mon_agent']['group']
+    owner node[:mon_agent][:owner]
+    group node[:mon_agent][:group']
     mode 0644
     variables(
-      :init_config => node[:mon_agent][:plugin][plugin][:init_config],
-      :instances => node[:mon_agent][:plugin][plugin][:instances]
+      init_config: node[:mon_agent][:plugin][plugin][:init_config],
+      instances: node[:mon_agent][:plugin][plugin][:instances]
     )
-    notifies :run, "execute[mon-setup]"
+    notifies :run, 'execute[mon-setup]'
   end
 end
