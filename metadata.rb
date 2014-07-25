0058373a3d06311971c9f8a4490b63fff8b1b5c5
Tim Kuhlman <tim.kuhlman@hp.com>
2014-07-25 11:46:17 -0600

    Fix for new keystoneclient requirements
    Change-Id: I88939df1f1fda8eae18ae74227bd38a3f4b64cff

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,6 @@ maintainer       'HP_Cloud_Monitoring'
 maintainer_email 'hpcs-mon@hp.com'
 description      'Installs/Configures mon-agent components'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.1.1'
+version          '1.1.2'
 depends          'python'
 recipe 'mon_api::default', 'Default'
