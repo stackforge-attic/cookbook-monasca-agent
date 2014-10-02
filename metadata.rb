df2d610796f28ff4b411986aeddf7f10f1c689f0
Tim Kuhlman <tim.kuhlman@hp.com>
2014-10-02 09:01:38 -0600

    Last commit did not bump version of the cookbook.
    Change-Id: I2350adbab1519cafd4308a6f309cca5055d9202d

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,6 @@ maintainer 'monasca'
 maintainer_email 'monasca@lists.launchpad.net'
 description 'Installs/Configures monasca-agent components'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.1.5'
+version          '1.1.6'
 depends          'python'
 recipe 'mon_api::default', 'Default'
